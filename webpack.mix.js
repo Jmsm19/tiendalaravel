462bd1ce7583852c7420499c71561e38071df157
Jorge Soto <Jorgemsm19@gmail.com>
2017-08-10 16:04:39 -0400

    Update Bootstrap to v4 alpha 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,14 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.autoload({
+  jquery: ['$', 'jQuery', 'window.jQuery'],
+  tether: ['Tether', 'window.Tether']
+});
+
+mix.js([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',  
+        'resources/assets/js/app.js'
+    ], 'public/js/app.js')
+   .sass('resources/assets/sass/app.scss', 'public/css/app.css');
