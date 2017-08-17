8cd215629b3b34fc56818050bcc58e6426aa1b78
Jorge Soto <Jorgemsm19@gmail.com>
2017-08-17 09:33:58 -0400

    Small tweaks to address form

--- resources/views/pages/inc/addressForm.blade.php
+++ resources/views/pages/inc/addressForm.blade.php
@@ -1,38 +1,38 @@
 {!! Form::open(array('route' => 'address.store', 'method' => 'POST', 'class' => 'form')) !!}
   <div class="form-group">
     {!! Form::label('name', 'Full name', ['class' => 'form-label']) !!}
-    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'John Doe']) !!}
+    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'John Doe', 'required']) !!}
   </div>
 
   <div class="form-group">
     {!! Form::label('address1', 'Street address', ['class' => 'form-label']) !!}
-    {!! Form::text('address1', null, ['class' => 'form-control', 'placeholder' => 'Street and number, P.O. box, etc.']) !!}
+    {!! Form::text('address1', null, ['class' => 'form-control', 'placeholder' => 'Street and number, P.O. box, etc.', 'required']) !!}
   </div>
 
   <div class="form-group">
-    {!! Form::text('address2', null, ['class' => 'form-control', 'placeholder' => 'Appartment, suite, unit, building, floor, etc.']) !!}
+    {!! Form::text('address2', null, ['class' => 'form-control', 'placeholder' => 'Appartment, suite, unit, building, floor, etc.', 'required']) !!}
   </div>
 
   <div class="form-group">
     {!! Form::label('country', 'Country', ['class' => 'form-label']) !!}
-    <select name="country" class="form-control">
+    <select name="country" class="form-control" required>
       @include('pages.inc.countryList')
     </select>
   </div>
 
   <div class="form-group">
     {!! Form::label('city', 'City', ['class' => 'form-label']) !!}
-    {!! Form::text('city', null, ['class' => 'form-control']) !!}
+    {!! Form::text('city', null, ['class' => 'form-control', 'required']) !!}
   </div>
 
   <div class="form-group">
     {!! Form::label('state', 'State / Province / Region', ['class' => 'form-label']) !!}
-    {!! Form::text('state', null, ['class' => 'form-control']) !!}
+    {!! Form::text('state', null, ['class' => 'form-control', 'required']) !!}
   </div>
 
   <div class="form-group">
     {!! Form::label('zip', 'Zip code', ['class' => 'form-label']) !!}
-    {!! Form::text('zip', null, ['class' => 'form-control']) !!}
+    {!! Form::text('zip', null, ['class' => 'form-control', 'required']) !!}
   </div>
 
   <div class="form-group">
