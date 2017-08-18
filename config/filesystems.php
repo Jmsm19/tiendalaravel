dde3b557a4ba8ef78326a773cc67a0afbae7ddef
Jorge Soto <Jorgemsm19@gmail.com>
2017-08-18 11:10:40 -0400

    Use aws/aws-sdk and s3 adapter

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,10 +57,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_KEY'),
-            'secret' => env('AWS_SECRET'),
-            'region' => env('AWS_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_REGION', 'us-east-1'),
+            'bucket' => env('S3_BUCKET'),
         ],
 
     ],
